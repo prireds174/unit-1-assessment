a5d94a8e824c99938c49ea75c3df1a1a23877e80
prireds174 <pri.reds174@gmail.com>
2022-01-10 17:54:37 -0500

    Partial Assessment

--- assessment-part-1/script.js
+++ assessment-part-1/script.js
@@ -1,1 +1,21 @@
-// Insert Javascript Here
+console.log('End of Unit 1')// Insert Javascript Here
+
+const plusButton = document.querySelector("#plus");
+const minusButton = document.querySelector("#minus");
+const valueInput = document.querySelector("#textBox")
+
+plusButton.addEventListener('click', () => {
+    console.log('add the number')
+})
+
+function addValue(){
+    let input = document.getElementById("textBox").value
+
+    
+
+}
+
+minusButton.addEventListener('click', () => {
+    console.log('subtract the number')
+})
+
--- part-2.js
+++ part-2.js
@@ -1,27 +1,45 @@
 // Make sure to RETURN all of your results
 
-const joinStrings = (str1, str2) => {
+//const joinStrings = (str1, str2) => {
   // Return a string with both strings combined. You MUST include a space between the two strings when joining.
 
+//
+function joinStrings(str1,str2) { return (str1 + " " + str2)
 }
+console.log(joinStrings('Hot', 'Potato'))
 
+/*
 const absoluteSum = (arrNums) => {
   // Return the absolute sum of all INTEGERS in an array
   // Note not all items in the array may be a number datatype
   // Remember to check the type of the element
 
-}
-
+}*/
+let num = 0
+function absoluteSum(arr) {
+  for (let i = 0; i < arr.length; i++){
+    num += arr[i]
+  }
+  return num
+} 
+console.log(absoluteSum([5,6,-7,8]))
+/*
 const returnAllValues = (object) => {
   // return all values from the provided object in a new array
 
-}
+}*/
+let values = ["Spaghetti", "Kitty", "Fireplace"];
+let returnAllValues = values;
 
-const countItems = (arr) => {
+console.log(returnAllValues)
+
+
+/*const countItems = (arr) => {
   // return total number of items in the provided array
   
 }
 
+/*
 const returnAllEvens = (nums) => {
   // return all even numbers in a new array from the provided nums array.
 
@@ -31,7 +49,12 @@ const returnPower = (num, pow) => {
   // return the provided num to the power provided
 
 }
-
+*/
+function returnPower(num,pow) {
+  return (Math.pow(num,pow))
+}
+console.log(returnPower(5,3))
+/*
 module.exports = {
   joinStrings,
   absoluteSum,
@@ -40,3 +63,4 @@ module.exports = {
   returnAllEvens,
   returnPower
 }
+*/
